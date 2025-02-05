7fd4845ab1f0dddcefaa4aafb48e34708891fa9c
kodri01 <kodrimukhtar5@gmail.com>
2025-02-05 11:53:22 +0700

    fix: update dashboard

--- app/Http/Controllers/Api/Admin/DashboardController.php
+++ app/Http/Controllers/Api/Admin/DashboardController.php
@@ -7,7 +7,11 @@ use App\Models\Aparatur;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
@@ -16,28 +20,52 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //count categories
-        $categories = Category::count();
+        $modelrole = DB::table('model_has_roles')->where('model_id', auth()->guard('api')->user()->id)->first();
+        $role = Role::where('id', $modelrole->role_id)->first();
 
-        //count posts
-        $posts = Post::count();
+        if ($role->name == 'admin') {
+            $categories = Category::count();
 
-        //count products
-        $products = Product::count();
+            //count posts
+            $posts = Post::count();
 
-        //count aparaturs
-        $aparaturs = Aparatur::count();
+            //count products
+            $products = Product::count();
 
-        //return response json
-        return response()->json([
-            'success'   => true,
-            'message'   => 'List Data on Dashboard',
-            'data'      => [
-                'categories' => $categories,
-                'posts'      => $posts,
-                'products'   => $products,
-                'aparaturs'  => $aparaturs,
-            ]
-        ]);
+            //count aparaturs
+            $aparaturs = Aparatur::count();
+
+            return response()->json([
+                'success'   => true,
+                'message'   => 'List Data on Dashboard',
+                'data'      => [
+                    'categories' => $categories,
+                    'posts'      => $posts,
+                    'products'   => $products,
+                    'aparaturs'  => $aparaturs,
+                ]
+            ]);
+        } else {
+            $posts = Post::where('user_id', Auth::user()->id)->count();
+
+            //count products
+            $products = Product::where('user_id', Auth::user()->id)->count();
+
+            $aparaturs = Aparatur::count();
+            $categories = Category::count();
+
+
+
+            return response()->json([
+                'success'   => true,
+                'message'   => 'List Data on Dashboard',
+                'data'      => [
+                    'categories' => $categories,
+                    'posts'      => $posts,
+                    'products'   => $products,
+                    'aparaturs'  => $aparaturs,
+                ]
+            ]);
+        }
     }
 }
